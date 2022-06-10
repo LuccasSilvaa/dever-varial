b7fc6e3420a12f05443e7f4a1767638e40ad7b2c
LuccasSilvaa <correaluccas@hotmail.com>
2022-06-09 22:34:44 -0300

    subir dever

---
+++ script.js
@@ -0,0 +1,15 @@
+let grupo3 = "Luccas"
+
+console.log (grupo3)
+
+let quantidade = 5;
+let anoAtual = 2022;
+
+console.log (anoAtual - quantidade);
+
+let texto = "Estou mexendo no console"
+console.log(texto);
+
+let quartaFeira = true;
+console.log(quartaFeira)
+console.log(typeof quartaFeira)
